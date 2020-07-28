24eafaa7e58d1915681e3bf85b3d8aaf2e1cf2b1
saifur703 <saifur703@gmail.com>
2020-07-28 19:50:04 +0600

    remove owl carousel

--- assets/js/active.js
+++ assets/js/active.js
@@ -2,18 +2,6 @@
   'use strict';
 
   jQuery(document).ready(function ($) {
-    // $('.testimonial_author').owlCarousel({
-    //     items: 1,
-    //     margin: 30,
-    //     loop: false,
-    //     dots: false,
-    //     nav: false,
-    //     navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
-    //     autoplay: false,
-    //     smartSpeed: 500,
-    //     animateOut: "fadeOut"
-    // });
-
     var strength = {
       0: 'Worst ☹',
       1: 'Bad ☹',
